9a349e98a2618377289c38e785a1f822d4161ebe
artpooja <artpooja19@gmail.com>
2022-02-27 11:22:02 +0530

    Settings.py Modified

--- WATD/settings.py
+++ WATD/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', """ (# whitenoise handles static files) """
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'WATD.urls'
